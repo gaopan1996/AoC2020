cb4031cebc7bf53f86591728607f84fbda33e390
Gao Pan <gaopan1996@gmail.com>
2020-12-04 13:45:35 -0500

    Add README and Day1

---
+++ AoC2020/README.md
@@ -0,0 +1,2 @@
+Second time doing AoC in year of 2020.
+Gave up early last year due to not having enough time. (Also got too hard). Will try to do more this year with more experience.
---
+++ AoC2020/day1/solution_2.py
@@ -0,0 +1,26 @@
+"""
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+
+Your puzzle answer was 176647680.
+
+Same concept as the previous part, but an additional loop
+"""
+with open('input.txt') as input_file:
+    lines = input_file.read().splitlines()
+    count = 0
+    while count < len(lines)-1:
+        expense = lines[count]
+        first_increment = count + 1
+        while first_increment < len(lines): 
+            expense_pair = lines[first_increment]
+            expense_trio = 2020 - int(expense) - int(expense_pair)
+            if str(expense_trio) in lines:
+                print(int(expense)*int(expense_pair)*expense_trio)
+                exit()
+            first_increment += 1
+        count += 1
